080c96a5cf9160d6c93b186cd9ce410c371898c1
davidreyes30 <davidreyesgonz@gmail.com>
2018-03-01 20:25:38 -0500

    construct2

---
+++ offline.js
@@ -0,0 +1,22 @@
+﻿{
+	"version": 1519953825,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"images/background-sheet0.png",
+		"images/door-sheet0.png",
+		"images/door-sheet1.png",
+		"images/player-sheet0.png",
+		"images/player-sheet1.png",
+		"images/hudarrow-sheet0.png",
+		"images/hudattack-sheet0.png",
+		"images/sprite-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
